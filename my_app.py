b5f579933867270c8ad98c7aa7291362a7fa3477
Timchikgus <163020704+Timchikgus@users.noreply.github.com>
2024-03-11 19:07:31 +0300

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,3 @@
+# напиши здесь код основного приложения и первого экрана
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton
